6418c39b037a6240177f45a0e9f65900751ef4f3
Yi-Wei Ang <yiweiang@TradeGeckos-MacBook-Air-2.local>
2016-04-12 20:08:32 +0800

    Added Nav Bar

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,21 @@
 $(function(){
   $('.container').hide();
   $('.container').fadeIn(1000);
+  
+  var main = main = $('.nav');
+  
+  $('.scroll').click(function(event){
+    event.preventDefault();
+    
+    var full_url = this.href,
+      parts = full_url.split('#'),
+      trgt = parts[1],
+      target_offset = $('#'+trgt).offset(),
+      target_top = target_offset.top;
+      
+    $('html,body').animate({scrollTop:target_top}, 500);
+    
+    main.children().removeClass();
+    $(this).parent().addClass('active');
+  })
 })
